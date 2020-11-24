62642ac6e959f0e3dff338908fd225f4e06f6c7d
2571263394@qq.com <2571263394@qq.com@example.com>
2020-11-24 22:12:15 +0800

    修改单例模板

--- Assets/Scripts/SingleIton.cs
+++ Assets/Scripts/SingleIton.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleIton <T>: MonoBehaviour where  T:MonoBehaviour
+public class SingleIton<T> : MonoBehaviour where T : MonoBehaviour
 {
     static T _ins;
     public static T _Ins { get { return _ins; } }
-
-   public virtual void Awake()
+    public virtual void Awake()
     {
         _ins = this as T;
     }
